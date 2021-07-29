03ec67b7b6173d44fab859455ef2d084165f5894
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-29 16:20:59 +0900

    ログイン画面を作成

--- Building7/LoginViewController.swift
+++ Building7/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Building7
 //
 //  Created by cmStudent on 2021/07/29.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
