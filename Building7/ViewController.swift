d97896c121a4f1483abea601385e21d46d09c3a5
Taguchi-wq <20cm0123@jec.ac.jp>
2021-07-29 16:12:58 +0900

    first commit

---
+++ Building7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Building7
+//
+//  Created by cmStudent on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
