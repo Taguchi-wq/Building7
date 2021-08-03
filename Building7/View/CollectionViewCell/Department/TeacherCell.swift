842a3250d41ce13340790211841de38e2216a452
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-03 23:25:59 +0900

    TeacherCellを作成

---
+++ Building7/View/CollectionViewCell/Department/TeacherCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherCell.swift
+//  Building7
+//
+//  Created by cmStudent on 2021/08/03.
+//
+
+import UIKit
+
+class TeacherCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
