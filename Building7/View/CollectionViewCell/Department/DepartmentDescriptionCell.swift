6514bf9072787f2b42cf59f5a8b26ddb1f9e6d6e
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-03 23:18:10 +0900

    DepartmentDescriptionCellを作成

---
+++ Building7/View/CollectionViewCell/Department/DepartmentDescriptionCell.swift
@@ -0,0 +1,17 @@
+//
+//  DepartmentDescriptionCell.swift
+//  Building7
+//
+//  Created by cmStudent on 2021/08/03.
+//
+
+import UIKit
+
+class DepartmentDescriptionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
