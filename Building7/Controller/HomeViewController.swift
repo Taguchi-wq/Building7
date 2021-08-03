5d7e5c7100531fa3205832bbd98c821e2c7fffe9
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-03 19:10:25 +0900

    ホーム画面の大枠なレイアウトを作成

--- Building7/Controller/HomeViewController.swift
+++ Building7/Controller/HomeViewController.swift
@@ -34,8 +34,70 @@ class HomeViewController: UIViewController {
     /// UICollectionViewの設定をする
     /// - Parameter collectionView: 設定するUICollectionView
     private func setupCollectionView(_ collectionView: UICollectionView) {
-        collectionView.dataSource = self
-        collectionView.registerFromNib(type: DepartmentCell.self)
+        collectionView.dataSource           = self
+        collectionView.collectionViewLayout = createHomeLayout()
+        collectionView.registerCell(type: DepartmentCell.self)
+        collectionView.registerReusableView(type: DepartmentHeder.self)
+    }
+    
+}
+
+
+// MARK: - Layout
+extension HomeViewController {
+    
+    // MARK: - Enums
+    /// フロアのセクション
+    private enum FloorsLayoutKind: CaseIterable {
+        case floor10, floor9, floor8, floor7, floor6, floor5, floor4, floor3, floor2, floor1, floorB1, floorB2
+    }
+    
+    
+    // MARK: - Private Funcs
+    /// ホーム画面のレイアウトを作成
+    /// - Returns: ホーム画面のレイアウト
+    private func createHomeLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let section = FloorsLayoutKind.allCases[sectionIndex]
+            switch section {
+            case .floor10: return self.floorSectionLayout()
+            case .floor9:  return self.floorSectionLayout()
+            case .floor8:  return self.floorSectionLayout()
+            case .floor7:  return self.floorSectionLayout()
+            case .floor6:  return self.floorSectionLayout()
+            case .floor5:  return self.floorSectionLayout()
+            case .floor4:  return self.floorSectionLayout()
+            case .floor3:  return self.floorSectionLayout()
+            case .floor2:  return self.floorSectionLayout()
+            case .floor1:  return self.floorSectionLayout()
+            case .floorB1: return self.floorSectionLayout()
+            case .floorB2: return self.floorSectionLayout()
+            }
+        }
+        return layout
+    }
+    
+    /// フロアのセクション
+    /// - Returns: フロアのセクション
+    private func floorSectionLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(3/5),
+                                               heightDimension: .fractionalHeight(1/5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let sectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .estimated(44))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sectionHeaderSize,
+                                                                        elementKind: UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .top)
+        section.boundarySupplementaryItems = [sectionHeader]
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 16
+        section.contentInsets = .init(top: 16, leading: 20, bottom: 16, trailing: 20)
+        return section
     }
     
 }
@@ -44,13 +106,22 @@ class HomeViewController: UIViewController {
 // MARK: - UICollectionViewDataSource
 extension HomeViewController: UICollectionViewDataSource {
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return FloorsLayoutKind.allCases.count
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return 3
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let departmentCell = collectionView.dequeueReusableCell(withReuseIdentifier: DepartmentCell.reuseIdentifier, for: indexPath) as! DepartmentCell
         return departmentCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let departmentHeder = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: DepartmentHeder.reuseIdentifier, for: indexPath) as! DepartmentHeder
+        return departmentHeder
     }
     
 }
--- Building7/Extension/UICollectionView+Extension.swift
+++ Building7/Extension/UICollectionView+Extension.swift
@@ -9,11 +9,19 @@ import UIKit
 
 extension UICollectionView {
     
-    /// UINibを登録する
-    func registerFromNib<T: UICollectionViewCell>(type: T.Type) {
+    // MARK: - Funcs
+    /// UICollectionViewCellを登録する
+    func registerCell<T: UICollectionViewCell>(type: T.Type) {
         let nibName = type.reuseIdentifier
         let nib = UINib(nibName: nibName, bundle: nil)
         register(nib, forCellWithReuseIdentifier: nibName)
+    }
+    
+    /// UICollectionReusableViewを登録する
+    func registerReusableView<T: UICollectionReusableView>(type: T.Type) {
+        let nibName = type.reuseIdentifier
+        let nib = UINib(nibName: nibName, bundle: nil)
+        register(nib, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: nibName)
     }
     
 }
