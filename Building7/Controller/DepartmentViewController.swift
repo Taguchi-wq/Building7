fb53fd079f375e19a2690eb56cd8bc5c105466df
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-03 19:43:31 +0900

    学科画面を作成

---
+++ Building7/Controller/DepartmentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DepartmentViewController.swift
+//  Building7
+//
+//  Created by cmStudent on 2021/08/03.
+//
+
+import UIKit
+
+class DepartmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
