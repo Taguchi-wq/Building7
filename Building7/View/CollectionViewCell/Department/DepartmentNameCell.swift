75821eee8d4996ac7be77d289caf87e8379c56c4
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-03 23:13:53 +0900

    DepartmentNameCellを作成

---
+++ Building7/View/CollectionViewCell/Department/DepartmentNameCell.swift
@@ -0,0 +1,17 @@
+//
+//  DepartmentNameCell.swift
+//  Building7
+//
+//  Created by cmStudent on 2021/08/03.
+//
+
+import UIKit
+
+class DepartmentNameCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
