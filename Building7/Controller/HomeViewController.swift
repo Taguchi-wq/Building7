9a7766c9167fce09f4509c8bf2eecd4a2e15609d
Taguchi-wq <20cm0123@jec.ac.jp>
2021-08-26 15:23:59 +0900

    学科がないクラスルームには遷移できないように変更

--- Building7/Controller/HomeViewController.swift
+++ Building7/Controller/HomeViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CoreMotion
 
 class HomeViewController: UIViewController {
     
@@ -61,6 +60,8 @@ class HomeViewController: UIViewController {
     
     /// 学科画面に遷移する
     private func presentDepartmentViewController(department: Department) {
+        // 学科がないクラスルームには遷移できないようにする
+        guard department.name != nil else { return }
         guard let departmentVC = storyboard?.instantiateViewController(withIdentifier: DepartmentViewController.reuseIdentifier) as? DepartmentViewController else { return }
         departmentVC.initialize(department: department)
         navigationController?.pushViewController(departmentVC, animated: true)
